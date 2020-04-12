a33b700f44de53cfe7b36ba2ec856efd94542033
Abhilash Vinod Kumar <hjoshi1123@gmail.com>
2020-04-12 16:46:17 -0500

    moved outside

---
+++ src/app/videoworkout/videoworkout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector : 'videoworkout',
+  templateUrl: './videoworkout.component.html',
+ // styleUrls: ['./videoworkout.component.css']
+})
+export class VideoworkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
