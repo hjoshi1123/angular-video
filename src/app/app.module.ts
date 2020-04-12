a5274fd379033520eb3ac09ccb67cf101a88386b
Abhilash Vinod Kumar <hjoshi1123@gmail.com>
2020-04-12 16:35:41 -0500

    video.js player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { VideoworkoutComponent } from './home-page/videoworkout/videoworkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    VideoworkoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-page/videoworkout/videoworkout.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './videoworkout.component.html',
+  styleUrls: ['./videoworkout.component.css']
+})
+export class VideoworkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
