90d93e81edfa9e61cc91a0620406f96121a957dd
Hrushikesh Joshi <hjoshi1123@gmail.com>
2020-04-12 16:56:37 -0500

    fix relocation error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { VideoworkoutComponent } from './home-page/videoworkout/videoworkout.component';
+import { VideoworkoutComponent } from './videoworkout/videoworkout.component';
 
 @NgModule({
   declarations: [
